c4a2ae01fc1e94f8f03e66740f762f1f567f752f
abdulrhmanGhandour <abdulrhmanghandour498@gmail.com>
2026-03-09 03:54:36 +0200

    MY HOSPITAL DONEEE

--- lib/features/Patient Hospital/Presentation/view/request_report_view.dart
+++ lib/features/Patient Hospital/Presentation/view/request_report_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:medi_cloud_app/features/Patient%20Hospital/Presentation/view/widgets/request_report_view_body.dart';
 
 class RequestReportView extends StatelessWidget {
   const RequestReportView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: RequestReportViewBody(),
+    );
   }
 }
